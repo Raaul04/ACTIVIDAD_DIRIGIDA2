cf46a5513f380d157088511b28a69e431c8df21d
Raaul04 <raulletrado@hotmail.com>
2024-11-29 13:33:46 +0100

    update

--- main.ts
+++ main.ts
@@ -16,7 +16,7 @@ await mongoClient.connect();
 
 console.info("Connected to MongoDB");
 
-const mongoDB = mongoClient.db("dinosaurs");
+const mongoDB = mongoClient.db("vuelos");
 const VuelosCollection = mongoDB.collection<VueloModel>("vuelos");
 
 
